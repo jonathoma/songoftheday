03a1e232099139a1c15a9bf69cdbd104bfc6d669
jonthoma <jonthoma@umich.edu>
2018-04-13 16:20:36 -0400

    refactored to separate authentication process

--- lastfm_recs.py
+++ lastfm_recs.py
@@ -7,6 +7,24 @@ import os
 from pylast import LastFMNetwork, PERIOD_7DAYS
 
 
+def authenticate():
+    """Authenticate to use LastFM API."""
+    try:
+        API_KEY = os.environ['LASTFM_API_KEY']
+        API_SECRET = os.environ['LASTFM_API_SECRET']
+        LASTFM_USERNAME = os.environ['LASTFM_USERNAME']
+        LASTFM_PASSWORD_HASH = os.environ['LASTFM_PASSWORD_HASH']
+        LASTFM_NETWORK = LastFMNetwork(api_key=API_KEY,
+                                       api_secret=API_SECRET,
+                                       username=LASTFM_USERNAME,
+                                       password_hash=LASTFM_PASSWORD_HASH)
+        user = LASTFM_NETWORK.get_user(LASTFM_USERNAME)
+    except KeyError:
+        print("Run setup.sh before trying this!")
+        exit(1)
+    return user
+
+
 def get_potential_recs(top_tracks):
     """Get potential recs from top tracks."""
     potential_recs = {}
@@ -20,23 +38,11 @@ def get_potential_recs(top_tracks):
 
 
 if __name__ == '__main__':
-    try:
-        API_KEY = os.environ['LASTFM_API_KEY']
-        API_SECRET = os.environ['LASTFM_API_SECRET']
-        lastfm_username = os.environ['LASTFM_USERNAME']
-        lastfm_password_hash = os.environ['LASTFM_PASSWORD_HASH']
-        lastfm_network = LastFMNetwork(api_key=API_KEY,
-                                       api_secret=API_SECRET,
-                                       username=lastfm_username,
-                                       password_hash=lastfm_password_hash)
-        user = lastfm_network.get_user(lastfm_username)
-        compare_factor = 12
-        top_tracks = user.get_top_tracks(period=PERIOD_7DAYS,
-                                         limit=compare_factor)
-        potential_recs = get_potential_recs(top_tracks)
-        for rec in potential_recs:
-            if potential_recs[rec] > 1:
-                print(rec.item)
-    except KeyError:
-        print("Run setup.sh before trying this!")
-        exit(1)
+    user = authenticate()
+    compare_factor = 12
+    top_tracks = user.get_top_tracks(period=PERIOD_7DAYS,
+                                     limit=compare_factor)
+    potential_recs = get_potential_recs(top_tracks)
+    for rec in potential_recs:
+        if potential_recs[rec] > 1:
+            print(rec.item)
--- spotify_recs.py
+++ spotify_recs.py
@@ -7,9 +7,26 @@ import spotipy
 import spotipy.util as util
 
 
-def get_potential_recs(results):
+def authenticate():
+    """Authenticate use of Spotify API."""
+    scope = 'user-top-read'
+    token = util.prompt_for_user_token(username, scope)
+    if not token:
+        print("Can't get token for", username)
+        exit(1)
+    sp = spotipy.Spotify(auth=token)
+    sp.trace = False
+    return sp
+
+
+def get_potential_recs(sp):
     """Get potential recs from Spotify."""
-    pass
+    results = sp.current_user_top_tracks(time_range='short_term',
+                                         limit=5)
+    uris = [track['uri'] for track in results['items']]
+    potential_recs = sp.recommendations(seed_tracks=uris, limit=10)
+    for track in potential_recs['tracks']:
+        print(track['artists'][0]['name'], '-', track['name'])
 
 
 if __name__ == '__main__':
@@ -18,19 +35,5 @@ if __name__ == '__main__':
     else:
         print("Usage: %s username" % (sys.argv[0],))
         exit(1)
-
-    scope = 'user-top-read'
-    token = util.prompt_for_user_token(username, scope)
-
-    if not token:
-        print("Can't get token for", username)
-        exit(1)
-
-    sp = spotipy.Spotify(auth=token)
-    sp.trace = False
-    results = sp.current_user_top_tracks(time_range='short_term',
-                                         limit=5)
-    uris = [track['uri'] for track in results['items']]
-    potential_recs = sp.recommendations(seed_tracks=uris, limit=10)
-    for track in potential_recs['tracks']:
-        print(track['artists'][0]['name'], '-', track['name'])
+    sp = authenticate()
+    get_potential_recs(sp)
